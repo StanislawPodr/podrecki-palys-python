fac67af3f839658e3a137ebb5b39a803e780a7eb
mrpalys <mrpalys@gmail.com>
2026-03-17 22:08:05 +0100

    zadanie 1a

---
+++ lab2/src/paragraph_count.py
@@ -0,0 +1,18 @@
+import sys
+
+def paragraph_count(stream=sys.stdin):
+    count = 0
+    is_paragraph_new = True
+
+    for line in stream:
+        if (line.strip() == ""): #Zmiana flagi jesli pusta linijka
+            is_paragraph_new = True
+        else:
+            if is_paragraph_new: #Jezeli ostatnia linia byla pusta to +1 paragraf
+                count += 1
+            is_paragraph_new = False
+    return count
+
+if __name__ == "__main__":
+    result = paragraph_count(sys.stdin)
+    print(result)
