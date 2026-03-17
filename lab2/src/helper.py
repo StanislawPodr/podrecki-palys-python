1f5b41d00c66a9950aaae95b1dcf419e7d193511
mrpalys <mrpalys@gmail.com>
2026-03-17 22:04:06 +0100

    2d i get_sentence w helper.py

--- lab2/src/helper.py
+++ lab2/src/helper.py
@@ -12,3 +12,29 @@ def get_line(input=sys.stdin):
     
     return line
 
+def get_sentence(input=sys.stdin):
+    sentence = ""
+    is_last_nl = False
+
+    while True:
+        c = input.read(1)
+
+        if not c:
+            if sentence.strip():
+                return sentence.strip()
+            raise EOFError
+
+        if c == "\n":
+            if is_last_nl:  #nowy akapit - zwroc zdanie jesli jest
+                if sentence.strip():
+                    return sentence.strip()
+                is_last_nl = False
+                sentence = ""
+            else:
+                is_last_nl = True
+                sentence += " " #zdanie dalej w nowej lini
+        else:
+            is_last_nl = False
+            sentence += c
+            if c in ".!?:":
+                return sentence.strip()
---
+++ lab2/src/longest_sentence.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin.reconfigure(encoding='utf-8') #polskie znaki
+from helper import get_sentence
+
+
+def find_longest_sentence(stream=sys.stdin):
+    longest = ""
+
+    try:
+        while True:
+            sentence = get_sentence(stream)
+            if len(sentence) > len(longest):
+                longest = sentence
+    except EOFError:
+        return longest
+
+
+if __name__ == "__main__":
+    result = find_longest_sentence()
+    print(result)
